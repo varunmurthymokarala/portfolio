6ba784fc8570e8c381a4eec7e0860858283a2de8
varunmurthymokarala <mokaralavarun@gmail.com>
2020-07-24 01:40:33 -0400

    Updates

---
+++ precache-manifest.a287dac5ec6a9c831564186a110006d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc210edc19d4aee05b61e8a71a1bd222",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "20ff7d4d722de2de8187",
+    "url": "/portfolio/static/css/main.d09940a3.chunk.css"
+  },
+  {
+    "revision": "9085aa51a7a7b877e38f",
+    "url": "/portfolio/static/js/2.f5112648.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio/static/js/2.f5112648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ff7d4d722de2de8187",
+    "url": "/portfolio/static/js/main.d088c1a6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "92a3951d2f0e2a3ce25c93c311d4811e",
+    "url": "/portfolio/static/media/bag.92a3951d.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/portfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "01db4b2374c5cd7c846c5dee80bf2dd7",
+    "url": "/portfolio/static/media/pp.01db4b23.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/portfolio/static/media/react.f370c1e0.png"
+  }
+]);
